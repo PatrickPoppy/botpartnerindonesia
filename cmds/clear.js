b705805d6dca55811509b23099245c7d98e7a3e5
Botpartnerindonesia <38397174+Botpartnerindonesia@users.noreply.github.com>
2018-04-15 17:30:40 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -27,8 +27,8 @@ exports.run = async (bot, message, args) => {
      let embed = new Discord.RichEmbed()
          .setTitle('Clean! :put_litter_in_its_place:  ')
          .setDescription(`${insert} messages deleted.`)
-        .setColor("#9A2EFE")
-         .setFooter('©Beta | By: Qyto | 2018')
+        .setColor("#FE2E2E")
+         .setFooter('© DISCORD BOT PARTNER INDONESIA.')
      message.channel.send(embed)
      message.delete();
   }
