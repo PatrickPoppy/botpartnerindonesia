c250ef6a03b2f2114849d6b175dc13a140664790
Botpartnerindonesia <38397174+Botpartnerindonesia@users.noreply.github.com>
2018-04-15 17:38:25 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
 let ping = Date.now() - message.createdTimestamp
 let embed = new Discord.RichEmbed()
-.setTitle('Your ping results:')
+.setTitle('Hasil Ping kamu:')
 .addField('Signal :signal_strength:',`${ping}ms`, true)
         .setColor("#FE2E2E")
          .setFooter('© DISCORD BOT PARTNER INDONESIA.')
